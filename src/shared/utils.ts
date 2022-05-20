a2aadc662353f6d674fadd22d78ae031c335a360
napthedev <phongna.dev@gmail.com>
2022-05-20 16:34:29 +0700

    Chore: remove unused utils

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,13 +1,3 @@
-export const formatVideoTime = (time: number) => {
-  const date = new Date(0);
-  date.setSeconds(time);
-  const timeString = date.toISOString().substr(11, 8);
-  return timeString;
-};
-
-export const isMobile = () =>
-  /iPhone|iPad|iPod|Android/i.test(window.navigator.userAgent);
-
 export const htmlToText = (html: string) => {
   const div = document.createElement("div");
   div.innerHTML = html;
