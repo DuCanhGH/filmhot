bfa6ef6f9c47dceb2acdaf5fc57dcf545a6d2327
DuCanhGH <75556609+DuCanhGH@users.noreply.github.com>
2022-08-16 22:54:34 +0700

    test

--- src/components/WatchView/MetaData.tsx
+++ src/components/WatchView/MetaData.tsx
@@ -1,4 +1,5 @@
-import { FC } from "react";
+import classNames from "classnames";
+import { FC, useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaRedoAlt } from "react-icons/fa";
 
@@ -11,6 +12,16 @@ interface MetaDataProps {
 }
 
 const MetaData: FC<MetaDataProps> = ({ data, episodeIndex }) => {
+  const lastEpisodeRef = useRef<HTMLAnchorElement | null>(null);
+  const [showLoadMoreButton, setShowLoadMoreButton] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
+  useEffect(() => {
+    if (lastEpisodeRef.current) {
+      if (lastEpisodeRef.current.offsetTop > 0) {
+        setShowLoadMoreButton(true);
+      }
+    }
+  }, []);
   return (
     <>
       {data ? (
@@ -53,19 +64,35 @@ const MetaData: FC<MetaDataProps> = ({ data, episodeIndex }) => {
           {data.episodeVo.length > 1 && (
             <>
               <h1 className="text-xl my-3">Episodes</h1>
-              <div className="flex max-w-[92vw] md:max-w-[calc(88vw-300px)] gap-3 overflow-auto">
-                {data.episodeVo.map((_, index) => (
-                  <Link
-                    to={`/tv/${data.id}?episode=${index}`}
-                    key={index}
-                    className={`px-4 py-[8px] bg-dark-lighten rounded hover:brightness-125 transition duration-300 ${
-                      index === episodeIndex ? "!bg-primary text-white" : ""
-                    }`}
-                  >
-                    {index + 1}
+              <div
+                className={classNames("flex flex-wrap gap-3 relative", {
+                  "before:absolute before:bg-gradient-to-b before:from-[#00000000] before:to-dark before:top-10 before:w-full before:left-0 before:h-8 max-h-[68px] overflow-hidden":
+                    showLoadMoreButton && !isExpanded,
+                })}
+              >
+                {new Array(data.episodeVo.length).fill("").map((_, index) => (
+                  <Link to={`/tv/${data.id}/${index}`} key={index}>
+                    <a
+                      {...(index === data.episodeVo.length - 1 ? { ref: lastEpisodeRef } : {})}
+                      className={classNames(
+                        "px-4 h-[42px] flex items-center bg-dark-lighten rounded hover:brightness-125 transition duration-300",
+                        {
+                          "!bg-primary text-white": index === episodeIndex,
+                        },
+                      )}
+                    >
+                      {index + 1}
+                    </a>
                   </Link>
                 ))}
               </div>
+              {showLoadMoreButton && (
+                <div>
+                  <button className="text-primary" onClick={() => setIsExpanded(!isExpanded)}>
+                    {isExpanded ? "Show less" : "Show more"}
+                  </button>
+                </div>
+              )}
             </>
           )}
         </div>
